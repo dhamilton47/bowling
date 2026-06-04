cb9ff704663441ba88ee0d0ed07fb718cf41035e
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2026-06-03 22:02:08 -0400

    Initial commit person app

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ("last_name", "first_name", "middle_name", "preferred_name", "suffix", "address")
+	search_fields = ("first_name", "middle_name", "last_name", "preferred_name", "suffix")
+	list_filter = ("suffix",)
