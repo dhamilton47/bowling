ceb768a901bc71e772257658aa9a8617ba6acf04
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2026-06-05 21:00:07 -0400

    Initial commit player app

--- player/admin.py
+++ player/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from player.models import Player
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ("person", "student", "is_high_school_bowler", "is_league_bowler", "is_tournament_bowler")
+	search_fields = ("person__first_name", "person__last_name")
+	list_filter = ("is_high_school_bowler", "is_league_bowler", "is_tournament_bowler")
